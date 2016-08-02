f00ffc0b893eab891cf5830801ab318bdbb8105c
nndun <nndun@ADMIN>
2016-08-02 22:47:47 +0700

    02/08

---
+++ ShipShop.Model/Models/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShipShop.Model.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string SenderName { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string SenderAddress { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string SenderMobile { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string ReceiverName { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string ReceiverAddress { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ReceiverMobile { set; get; }
+
+        [MaxLength(256)]
+        public string PaymentMethod { set; get; }
+
+        public DateTime? CreatedDate { set; get; }
+        public string CreatedBy { set; get; }
+        public string PaymentStatus { set; get; }
+        public bool Status { set; get; }
+
+        public virtual IEnumerable<OrderDetail> OrderDetails { set; get; }
+    }
+}
---
+++ ShipShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShipShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        //[Column(Order = 1)]
+        public int OrderID { set; get; }
+
+        public int Quantitty { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; }
+    }
+}
