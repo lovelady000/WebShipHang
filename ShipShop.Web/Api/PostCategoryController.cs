376e2d5a7ea31ef7d53d3c2777f20a190d77690b
nndun <nndun@ADMIN>
2016-08-04 13:54:21 +0700

    Bower  config

--- ShipShop.Web/Api/PostCategoryController.cs
+++ ShipShop.Web/Api/PostCategoryController.cs
@@ -1,10 +1,5 @@
-﻿using AutoMapper;
-using ShipShop.Model.Models;
-using ShipShop.Service;
+﻿using ShipShop.Service;
 using ShipShop.Web.Infrastructure.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -14,15 +9,15 @@ namespace ShipShop.Web.Api
     [RoutePrefix("api/postcategory")]
     public class PostCategoryController : ApiControllerBase
     {
-        IPostCategoryService _postCategoryService;
+        private IPostCategoryService _postCategoryService;
 
-        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) :base(errorService)
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) : base(errorService)
         {
             this._postCategoryService = postCategoryService;
         }
+
         //public PostCategoryController()
         //{
-
         //}
         [Route("getall")]
         public HttpResponseMessage Get(HttpRequestMessage request)
@@ -34,7 +29,6 @@ namespace ShipShop.Web.Api
                 //var listPostCategoryVM = Mapper.Map<List<PostCategoryViewModel>>(listCategory);
 
                 HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
-
 
                 return response;
             });
