3a33f51615de37c85b7872c48561a1ee730b4443
lovelady000 <acquybongdem00@gmail.com>
2016-08-25 16:54:07 +0700

    17h 25-08 commit 2

---
+++ ShipShop.Web/Resource/CommandSQL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShipShop.Web.Resource
+{
+    public class CommandSQL
+    {
+    }
+
+    //select r.Name
+    //from dbo.ApplicationRoles as r, dbo.ApplicationUserRoles as ur, dbo.ApplicationUsers as u
+    //where r.Id = ur.RoleId and u.Id = ur.UserId and u.UserName = ''
+}
