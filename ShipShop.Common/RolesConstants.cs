4c045c9fe8e6a3b55c5c492282021b2e98e2da94
lovelady000 <acquybongdem00@gmail.com>
2016-08-27 15:55:33 +0700

    COMMIT 16H 27-08 LAN 2

---
+++ ShipShop.Common/RolesConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipShop.Common
+{
+    public class RolesConstants
+    {
+        public const string ROLES_ADMIN = "Admin";
+        public const string ROLES_USER = "User";
+        public const string ROLES_ADD_NEW_USER = "add_new_user";
+        public const string ROLES_EDIT_USER = "edit_user";
+        public const string ROLES_FULL_CONTROL = "full_control";
+    }
+}
