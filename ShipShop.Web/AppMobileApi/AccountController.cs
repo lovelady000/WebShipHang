2725969edb3acc0815ced0e335f1d16a2a5ebf29
lovelady000 <acquybongdem00@gmail.com>
2016-09-19 19:31:59 +0700

    17h 19-09

---
+++ ShipShop.Web/AppMobileApi/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShipShop.Web.AppMobileApi
+{
+    [RoutePrefix("api/app/account")]
+    public class AccountController : ApiController
+    {
+
+    }
+}
